9d303fabe7ffdea189ec9152d9df9897d415bd2f
MiaMou <19845509@qq.com>
2021-01-13 19:18:44 +0800

    init

---
+++ src/jquery.js
@@ -0,0 +1,58 @@
+window.jQuery = function(selectorOrArray){
+    let elements
+    if(typeof selectorOrArray === 'string'){
+        elements = document.querySelectorAll(selectorOrArray)
+    }else if(selectorOrArray instanceof Array){
+        elements = selectorOrArray
+    }  
+    // api 可以操作 elements
+    return {
+        find(selector){
+            let array = []
+            for(let i=0; i<elements.length; i++){
+                array = array.concat(Array.from(elements[i].querySelectorAll(selector)))
+            }
+            array.oldApi = this  // this是api1          
+            return jQuery(array)
+        },
+        each(fn){
+            for(let i=0; i<elements.length; i++){
+                fn.call(null, elements[i], i)
+            }
+            return this // this就是当前api
+        },
+        parent(){
+            const array =[]
+            this.each((node)=>{
+                if(array.indexOf(node.parentNode) === -1){
+                    array.push(node.parentNode)
+                }            
+            })
+            return jQuery(array)
+        },
+        children(){
+            const array =[]
+            this.each((node)=>{
+                if(array.indexOf(node.parentNode) === -1){
+                    array.push(...node.children)  // ... 就是把children放到一个数组里
+                }            
+            })
+            return jQuery(array)
+        },
+        print() {
+            console.log(elements);
+        },
+        // 闭包：函数 addClass 访问了外部的变量 elements
+        addClass(className){
+            for(let i=0; i<elements.length; i++){
+                elements[i].classList.add(className)
+            }
+            return this  // 同return api         
+        },   
+        oldApi: selectorOrArray.oldApi,
+        end(){
+            return this.oldApi // this 就是当前的api(api2)
+        }
+
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// const api = jQuery('.test')  // 不返回元素们，返回 api 对象
+// // api.addClass('red').addClass('blue')   // 遍历所有刚才获取的元素 添加class , 通过 返回对象 链式操作
+// api.addClass('red')
+//    .addClass('blue')
+//    .addClass('green') // 链式操作
+
+
+// const api1 = jQuery('.test')
+// api1.addClass('blue')
+// const api2 = api1.find('.child').addClass('red')
+// api1.addClass('green')
+
+// jQuery('.test')
+//   .find('.child')
+//   .addClass('red')
+//   .addClass('blue')
+//   .addClass('green')
+
+
+// const api1 = jQuery('.test')
+// const api2 = api1.find('.child').addClass('red').addClass('blue').addClass('green')
+// const oldApi = api2.end().addClass('yellow')
+
+// const x = jQuery('.test').find('.child')
+// x.each((div)=>console.log(div))
+
+
+const x = jQuery('.test')
+x.parent().print()
+x.children().print()
